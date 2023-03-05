43dea18d0e096fe999cbe193fdfe270af39cc9cb
Nirmala <nirmalawickramasinghe16@gmail.com>
2023-03-05 15:57:52 +0530

    Updated!

--- src/student_details.java
+++ src/student_details.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class student_details {
+public class student_details {
+    public static void main(String[] args) {
+        System.out.println("Hello Student!!");
+
+    }
 }
